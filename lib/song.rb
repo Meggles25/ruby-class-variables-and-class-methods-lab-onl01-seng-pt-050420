a28782721b3c2ac30d0339b4d23481fc50aeda15
Megan Gomes <megan.gomes25@gmail.com>
2020-05-27 19:25:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,11 @@ class Song
   attr_accessor :name, :artist, :assert_generates
     @@count = 0 
     @@artists = []
-    @@genres = [
-      
-    ]
+    @@genres = []
+    
+  def initialize(song_name, artist, genre)
+    @@name = song_name
+    @@artist = artist 
+    @@genre = genre
+    @@count += 1
+    @@
